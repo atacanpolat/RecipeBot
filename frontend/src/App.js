c369a580b68c8445bfc5c952f87a59c6a152d637
Emanuele Salonico <esalonico@gmail.com>
2023-06-09 15:02:45 +0200

    implemented private sidebar and top header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,36 @@
-import logo from './logo.svg';
-import './assets/css/App.css';
-import 'react-toastify/dist/ReactToastify.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import logo from "./logo.svg";
+import "./assets/css/App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 // import Hero from './components/deprecated/Hero_tw';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Header from "./components/Header"
-import { ToastContainer } from 'react-toastify';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Generate from "./pages/Generate";
+
 function App() {
   return (
     <>
-    <Router>
-    <div className="container">
-      <Routes>
-        <Route path='/' element={<Homepage />}/>
-        <Route path='/login' element={<SignIn />}/>
-        <Route path='/register' element={<SignUp />}/>
-      </Routes>
-      <ToastContainer/>
-
-    </div>
-    </Router>
-   
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
+            {/* <Route path='/generate' element={<Generate />}/> */}
+          </Routes>
+          <ToastContainer />
+        </div>
+        <div className="container-private">
+          <Routes>
+            <Route path="/generate" element={<Generate />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,95 +1,96 @@
-import React from 'react'
-import EggAltIcon from '@mui/icons-material/EggAlt';
-import Button from '@material-ui/core/Button';
-import logo from '../images/logo.png'
-import { makeStyles } from '@material-ui/core/styles';
-import { purple, red } from '@mui/material/colors';
-import theme from './helpers/themes';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import EggAltIcon from "@mui/icons-material/EggAlt";
+import Button from "@material-ui/core/Button";
+import logo from "../images/logo.png";
+import { makeStyles } from "@material-ui/core/styles";
+import { purple, red } from "@mui/material/colors";
+import theme from "./helpers/themes";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
   primaryLink: {
-    [theme.breakpoints.up('lg')]: {
-      margin: '0',
+    [theme.breakpoints.up("lg")]: {
+      margin: "0",
     },
-    padding: '8px 24px',
-    borderRadius: '9999px',
-    backgroundColor:  theme.palette.violet.light,
+    padding: "8px 24px",
+    borderRadius: "9999px",
+    backgroundColor: theme.palette.violet.light,
     color: theme.palette.common.white,
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.violet.dark,
       color: theme.palette.grey[200],
     },
-    '&:focus': {
+    "&:focus": {
       boxShadow: `0 0 0 3px ${theme.palette.primary.light}`,
     },
-    borderBottom: 'none',
+    borderBottom: "none",
   },
   navLink: {
-    fontSize: '1.125rem',
-    margin: '0.5rem 0',
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '0.875rem',
-      margin: '0',
+    fontSize: "1.125rem",
+    margin: "0.5rem 0",
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "0.875rem",
+      margin: "0",
     },
-    fontWeight: '600',
-    letterSpacing: '0.05em',
-    transition: 'duration 300ms',
-    paddingBottom: '0.25rem',
-    borderBottom: '2px solid transparent',
-    '&:hover': {
+    fontWeight: "600",
+    letterSpacing: "0.05em",
+    transition: "duration 300ms",
+    paddingBottom: "0.25rem",
+    borderBottom: "2px solid transparent",
+    "&:hover": {
       color: theme.palette.primary.main,
     },
   },
   logoLink: {
-    display: 'flex',
-    alignItems: 'center',
-    fontWeight: '900',
-    borderBottom: 'none',
-    fontSize: '2rem',
-    href:'/',
-    marginLeft: '0',
-    '& img': {
-      width: '2.5rem',
-      marginRight: '0.75rem',
+    display: "flex",
+    alignItems: "center",
+    fontWeight: "900",
+    borderBottom: "none",
+    fontSize: "2rem",
+    href: "/",
+    marginLeft: "0",
+    "& img": {
+      width: "2.5rem",
+      marginRight: "0.75rem",
     },
   },
 }));
-
 
 function Header() {
   const classes = useStyles(theme);
 
   return (
-    
-    <header className='header'>
-        <div className='logo'>
-           <a className={`${classes.navLink} ${classes.logoLink}`}>
-              <img src={logo} alt="Logo" />
-              <Link to='/'>RecipeBot</Link>
-           </a>
-        </div>
-        <ul>
-            <li>
-              <Button className={classes.navLink} variant='text' href='/login'>Login</Button>
-            </li>
-            <li>
-            <Button className={classes.navLink} variant='text' href='/register'>Register</Button>
-            </li>
-            <li>
-            <Button 
-            className={classes.primaryLink} 
-            variant="contained" 
+    <header className="header">
+      <div className="logo">
+        <a className={`${classes.navLink} ${classes.logoLink}`}>
+          <img src={logo} alt="Logo" />
+          <Link to="/">RecipeBot</Link>
+        </a>
+      </div>
+      <ul>
+        <li>
+          <Button className={classes.navLink} variant="text" href="/login">
+            Login
+          </Button>
+        </li>
+        <li>
+          <Button className={classes.navLink} variant="text" href="/register">
+            Register
+          </Button>
+        </li>
+        <li>
+          <Button
+            className={classes.primaryLink}
+            variant="contained"
             startIcon={<EggAltIcon />}
-            href='/generate'>
-              Generate Recipe
-            </Button>            
-            </li>
-        </ul>
+            href="/generate"
+          >
+            Generate Recipe
+          </Button>
+        </li>
+      </ul>
     </header>
-  )
+  );
 }
 
-
-export default Header
+export default Header;
